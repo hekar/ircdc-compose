a7b4922cc626c5c0eb99a206836b19b5f93e2592
Hekar Khani <hekark@gmail.com>
2016-07-02 23:41:04 -0400

    Fixed Kafka container restart

--- start-services.sh
+++ start-services.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo docker-compose stop && sudo docker-compose up postgres kafka zookeeper
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sudo rm -f $DIR/data/*
+sudo docker-compose stop && sudo docker-compose up postgres kafka zookeeper ircd
